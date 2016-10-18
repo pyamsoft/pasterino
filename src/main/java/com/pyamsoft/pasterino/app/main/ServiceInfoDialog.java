a7a80a2a9691f94a604a815a47b0ab2e93d9cb32
pyamsoft <pyam.soft@gmail.com>
2016-10-17 17:17:37 -0700

    Lint and findbugs

--- src/main/java/com/pyamsoft/pasterino/app/main/ServiceInfoDialog.java
+++ src/main/java/com/pyamsoft/pasterino/app/main/ServiceInfoDialog.java
@@ -26,9 +26,7 @@ public class ServiceInfoDialog extends DialogFragment {
 
   @NonNull @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
     return new AlertDialog.Builder(getActivity()).setMessage("Pasterino service is On")
-        .setPositiveButton("Okay", (dialogInterface, i) -> {
-          dismiss();
-        })
+        .setPositiveButton("Okay", (dialogInterface, i) -> dismiss())
         .create();
   }
 }
