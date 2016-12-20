3adf8bc77dc2a0ec7f6265f0b4edb979b6723b1e
pyamsoft <pyam.soft@gmail.com>
2016-12-19 20:36:24 -0800

    Convert module to a full project

---
+++ pasterino/src/main/java/com/pyamsoft/pasterino/app/main/MainActivity.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.pasterino.app.main;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.preference.PreferenceManager;
+import android.view.MenuItem;
+import com.pyamsoft.pasterino.BuildConfig;
+import com.pyamsoft.pasterino.R;
+import com.pyamsoft.pasterino.databinding.ActivityMainBinding;
+import com.pyamsoft.pydroid.util.AppUtil;
+import com.pyamsoft.pydroidui.about.AboutLibrariesFragment;
+import com.pyamsoft.pydroidui.rating.RatingDialog;
+import com.pyamsoft.pydroidui.sec.TamperActivity;
+
+public class MainActivity extends TamperActivity {
+
+  private ActivityMainBinding binding;
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    setTheme(R.style.Theme_Pasterino_Light);
+    super.onCreate(savedInstanceState);
+    PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+    setupAppBar();
+  }
+
+  @Override protected int bindActivityToView() {
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    return R.id.ad_view;
+  }
+
+  @Override protected void onPostResume() {
+    super.onPostResume();
+    RatingDialog.showRatingDialog(this, this);
+  }
+
+  @NonNull @Override protected String getSafePackageName() {
+    return "com.pyamsoft.pasterino";
+  }
+
+  @Override protected void onStart() {
+    super.onStart();
+    showMainFragment();
+  }
+
+  @Override protected void onDestroy() {
+    super.onDestroy();
+    binding.unbind();
+  }
+
+  @Override public void onBackPressed() {
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    final int backStackCount = fragmentManager.getBackStackEntryCount();
+    if (backStackCount > 0) {
+      fragmentManager.popBackStack();
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override public boolean onOptionsItemSelected(final @NonNull MenuItem item) {
+    final int itemId = item.getItemId();
+    boolean handled;
+    switch (itemId) {
+      case android.R.id.home:
+        onBackPressed();
+        handled = true;
+        break;
+      default:
+        handled = false;
+    }
+    return handled || super.onOptionsItemSelected(item);
+  }
+
+  private void setupAppBar() {
+    setSupportActionBar(binding.mainToolbar);
+    binding.mainToolbar.setTitle(R.string.app_name);
+    ViewCompat.setElevation(binding.mainToolbar, AppUtil.convertToDP(this, 4));
+  }
+
+  private void showMainFragment() {
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    if (fragmentManager.findFragmentByTag(MainSettingsPreferenceFragment.TAG) == null
+        && fragmentManager.findFragmentByTag(AboutLibrariesFragment.TAG) == null) {
+      fragmentManager.beginTransaction()
+          .replace(R.id.main_container, new MainSettingsFragment(), MainSettingsFragment.TAG)
+          .commitNow();
+    }
+  }
+
+  @NonNull @Override protected String[] getChangeLogLines() {
+    final String line1 = "CHANGE: Some new settings to play with";
+    return new String[] { line1 };
+  }
+
+  @NonNull @Override protected String getVersionName() {
+    return BuildConfig.VERSION_NAME;
+  }
+
+  @Override public int getApplicationIcon() {
+    return R.mipmap.ic_launcher;
+  }
+
+  @NonNull @Override public String provideApplicationName() {
+    return "Pasterino";
+  }
+
+  @Override public int getCurrentApplicationVersion() {
+    return BuildConfig.VERSION_CODE;
+  }
+}
---
+++ pasterino/src/main/java/com/pyamsoft/pasterino/app/main/MainSettingsPreferenceFragment.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2016 Peter Kenji Yamanaka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pyamsoft.pasterino.app.main;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.CheckResult;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.preference.Preference;
+import android.view.View;
+import com.pyamsoft.pasterino.Pasterino;
+import com.pyamsoft.pasterino.R;
+import com.pyamsoft.pasterino.app.service.PasteService;
+import com.pyamsoft.pasterino.app.service.PasteServiceNotification;
+import com.pyamsoft.pasterino.app.service.SinglePasteService;
+import com.pyamsoft.pydroid.app.PersistLoader;
+import com.pyamsoft.pydroid.util.AppUtil;
+import com.pyamsoft.pydroid.util.PersistentCache;
+import com.pyamsoft.pydroidui.about.AboutLibrariesFragment;
+import com.pyamsoft.pydroidui.app.fragment.ActionBarSettingsPreferenceFragment;
+import timber.log.Timber;
+
+public class MainSettingsPreferenceFragment extends ActionBarSettingsPreferenceFragment
+    implements MainSettingsPreferencePresenter.MainSettingsView {
+
+  @NonNull public static final String TAG = "MainSettingsPreferenceFragment";
+  @NonNull private static final String KEY_PRESENTER = "key_main_presenter";
+  @SuppressWarnings("WeakerAccess") MainSettingsPreferencePresenter presenter;
+  private long loadedKey;
+
+  @CheckResult @NonNull MainSettingsPreferencePresenter getPresenter() {
+    if (presenter == null) {
+      throw new NullPointerException("Presenter is NULL");
+    }
+
+    return presenter;
+  }
+
+  @NonNull @Override protected AboutLibrariesFragment.BackStackState isLastOnBackStack() {
+    return AboutLibrariesFragment.BackStackState.LAST;
+  }
+
+  @Override protected int getRootViewContainer() {
+    return R.id.main_container;
+  }
+
+  @NonNull @Override protected String getApplicationName() {
+    return getString(R.string.app_name);
+  }
+
+  @Override protected int getPreferenceXmlResId() {
+    return R.xml.preferences;
+  }
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    loadedKey = PersistentCache.get()
+        .load(KEY_PRESENTER, savedInstanceState,
+            new PersistLoader.Callback<MainSettingsPreferencePresenter>() {
+              @NonNull @Override
+              public PersistLoader<MainSettingsPreferencePresenter> createLoader() {
+                return new MainSettingsPreferencePresenterLoader();
+              }
+
+              @Override
+              public void onPersistentLoaded(@NonNull MainSettingsPreferencePresenter persist) {
+                presenter = persist;
+              }
+            });
+  }
+
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    final Preference explain = findPreference(getString(R.string.explain_key));
+    explain.setOnPreferenceClickListener(preference -> {
+      AppUtil.guaranteeSingleDialogFragment(getFragmentManager(), new HowToDialog(), "howto");
+      return true;
+    });
+  }
+
+  @Override public void onStart() {
+    super.onStart();
+    presenter.bindView(this);
+  }
+
+  @Override public void onStop() {
+    super.onStop();
+    presenter.unbindView();
+  }
+
+  @Override public void onDestroy() {
+    super.onDestroy();
+    if (!getActivity().isChangingConfigurations()) {
+      PersistentCache.get().unload(loadedKey);
+    }
+    Pasterino.getRefWatcher(this).watch(this);
+  }
+
+  @Override protected boolean onClearAllPreferenceClicked() {
+    presenter.clearAll();
+    return super.onClearAllPreferenceClicked();
+  }
+
+  @Override public void onSaveInstanceState(Bundle outState) {
+    PersistentCache.get()
+        .saveKey(outState, KEY_PRESENTER, loadedKey, MainSettingsPreferencePresenter.class);
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override public void showConfirmDialog() {
+    AppUtil.guaranteeSingleDialogFragment(getFragmentManager(), new ConfirmationDialog(),
+        "confirm");
+  }
+
+  @Override public void onClearAll() {
+    PasteServiceNotification.stop(getContext());
+    SinglePasteService.stop(getContext());
+    try {
+      PasteService.finish();
+    } catch (NullPointerException e) {
+      Timber.e(e, "Expected exception when Service is NULL");
+    }
+
+    final ActivityManager activityManager = (ActivityManager) getContext().getApplicationContext()
+        .getSystemService(Context.ACTIVITY_SERVICE);
+    activityManager.clearApplicationUserData();
+  }
+}
